923ddc75648905c4e5a2bd1597e7b119c1278f5e
Misikir Abate Teka <41914268+misikir21@users.noreply.github.com>
2023-06-25 11:35:37 +0300

    Update server.js

--- sample/server.js
+++ sample/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/string'});
    let dt = new Date();
-   response.end("This is a new string returned on "+ dt.toDateString()+" at "+dt.toLocaleTimeString('en-US'))
+   response.end("This is a new string returned as modified by misikir teka on "+ dt.toDateString()+" at "+dt.toLocaleTimeString('en-US'))
 }).listen(8080);
 
 console.log('Server running at http://0.0.0.0:8080/');
